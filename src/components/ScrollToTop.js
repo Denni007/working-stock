39b791042d0e4982c6f610d0f841d796b1698139
Denni007 <kukadiyadenish@gmail.com>
2023-02-07 23:14:41 +0530

    first commit

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -6,7 +6,9 @@ import { useLocation } from 'react-router-dom';
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
-  useEffect(() => {
+  useEffect(() => {  
+
+    
     window.scrollTo(0, 0);
   }, [pathname]);
 
